134a8848d26f41b0b150199ac0f86c52582efd3a
Anton Usmansky <cody0@yandex-team.ru>
2016-03-03 13:37:30 +0300

    Fail all suites tests if test not specified

--- test/plugin.js
+++ test/plugin.js
@@ -14,17 +14,17 @@ describe('gemini-teamcity', function() {
         gemini, runner;
 
     function stubEventData_(opts) {
-        return _.defaults(opts || {}, {
+        return _.extend({
             suite: {
-                fullName: 'Suite full name'
+                fullName: 'Suite default full name'
             },
             state: {
-                name: 'State name'
+                name: 'State default name'
             },
-            browserId: 'Firefox',
-            sessionId: 'sessionId',
+            browserId: 'default-browser',
+            sessionId: 'default-session-id',
             equal: true
-        });
+        }, opts);
     }
 
     beforeEach(function() {
@@ -42,8 +42,6 @@ describe('gemini-teamcity', function() {
     });
 
     function testArgs_(event, handleMethod, specificData) {
-        specificData = specificData || {};
-
         it('should call "' + event + '" with proper full test name', function() {
             var data = _.extend({
                     browserId: 'some-browser',
@@ -89,26 +87,72 @@ describe('gemini-teamcity', function() {
     });
 
     describe('on error', function() {
-        it('should call "testFailed" with stack and message', function() {
-            runner.emit('err', stubEventData_({
-                stack: 'error stack',
-                message: 'error message'
-            }));
+        describe('with state', function() {
+            it('should call "testFailed" with stack and message', function() {
+                runner.emit('err', stubEventData_({
+                    stack: 'error stack',
+                    message: 'error message'
+                }));
 
-            assert.calledOnce(tsm.testFailed);
-            assert.calledWithMatch(tsm.testFailed, {
-                details: 'error stack',
-                message: 'error message'
+                assert.calledOnce(tsm.testFailed);
+                assert.calledWithMatch(tsm.testFailed, {
+                    details: 'error stack',
+                    message: 'error message'
+                });
+            });
+
+            it('should call "testFinished"', function() {
+                runner.emit('err', stubEventData_({
+                    stack: 'error stack',
+                    message: 'error message'
+                }));
+
+                assert.calledOnce(tsm.testFinished);
             });
         });
 
-        it('should call "testFinished"', function() {
-            runner.emit('err', stubEventData_({
-                stack: 'error stack',
-                message: 'error message'
-            }));
+        describe('without state', function() {
+            var state1, state2, suite, data;
 
-            assert.calledOnce(tsm.testFinished);
+            beforeEach(function() {
+                state1 = {name: 'state1'},
+                state2 = {name: 'state2'},
+                suite = {
+                    fullName: 'some suite',
+                    states: [state1, state2]
+                },
+                data = stubEventData_({
+                    state: undefined,
+                    suite: suite,
+                    browserId: 'some-browser'
+                });
+            });
+
+            it('should fail all suite states', function() {
+                runner.emit('err', data);
+
+                assert.calledTwice(tsm.testFailed);
+                assert.calledWithMatch(tsm.testFailed, {name: 'some_suite.state1.some-browser'});
+                assert.calledWithMatch(tsm.testFailed, {name: 'some_suite.state2.some-browser'});
+            });
+
+            it('should not fail already finished suite states', function() {
+                runner.emit('endTest', _.defaults({state: state1}, data));
+                runner.emit('endTest', _.defaults({state: state2, equal: false}, data));
+
+                runner.emit('err', data);
+
+                assert.calledOnce(tsm.testFailed);
+            });
+
+            it('should not fail skipped states', function() {
+                runner.emit('skipState', _.defaults({state: state1}, data));
+                runner.emit('skipState', _.defaults({state: state2}, data));
+
+                runner.emit('err', data);
+
+                assert.notCalled(tsm.testFailed);
+            });
         });
     });
 });
